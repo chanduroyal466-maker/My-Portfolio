63e067155c43f1c5008d6f53b93b221136a7680b
P.S.Chandana Royal <chanduroyal466@gmail.com>
2026-08-11 19:51:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+    alert("Welcome to my portfolio! 🚀");
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
